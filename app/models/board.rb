e5c6f3941a41a4a609d04ec735d79a36723c6bb6
Alex Berry <highwaybobbery@gmail.com>
2014-02-06 00:10:04 -0500

    updated board, green specs!

--- app/models/board.rb
+++ app/models/board.rb
@@ -25,33 +25,12 @@ class Board
       @cells.select.with_index { |_,x| x % 9 == column }
     end
 
-    @squares = (0...9).map do |square|
-      @rows.map do |row|
-        row.slice(square, 3)
-      end
+    @squares = [[],[],[],[],[],[],[],[],[]]
+    @cells.each_slice(3).each_with_index do |three, index|
+      col =  ((index) % 3) + 1
+      row = (index / 9) + 1
+      @squares[(col * row) - 1].concat three
     end
-  end
-
-  def generate
-    doc = Nokogiri::HTML(open('http://www.free-sudoku.com/sudoku.php'))
-
-    boardarray = []
-    i = 0
-
-    until i == 82
-      boardarray << doc.css("##{i}").text
-      i+=1
-    end
-
-    boardarray.map! do |cell|
-      if cell == ''
-        cell = '0'
-      else
-        cell = cell
-      end
-    end
-
-    boardarray.join
   end
 
 end
